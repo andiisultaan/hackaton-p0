f78cad1bf2ecd5da4e9982651b47ebe4372d614e
ravizp <ravi.zaraska@gmail.com>
2024-08-14 15:50:59 +0700

    card.js

---
+++ js/card.js
@@ -0,0 +1,28 @@
+let datapokemon = {
+    idpokemon:"", //number
+    nama_pokemon:"", //string
+    keterangan:"", //string
+    stok:"", //number
+    harga_modal:"", //number
+    harga_jual:"",//number
+    gambar:"", //img
+    
+}
+
+let datapelanggan = {
+    idpelanggan: "", //string
+    namapelanggan: "", //string
+    no_tlp: "", //string
+    email: "", //email 
+    alamat: ""// string
+}
+
+let datatransaksi ={
+    idtransaksi:"", //number
+    idpokemon:"", //date
+    idbarang: "", //number
+    tgl_transaksi: "", //date
+    totalbarang: "", // number
+    totalbayar: "", // string
+    status: "", //string
+}
